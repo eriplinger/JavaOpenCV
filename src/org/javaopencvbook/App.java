6990a03acd5308cc845af4f1d9c9b60aff643abf
eriplinger <eriplinger@googlemail.com>
2016-05-28 11:49:42 +0200

    Grünerkennung implementiert

--- src/org/javaopencvbook/App.java
+++ src/org/javaopencvbook/App.java
@@ -1,7 +1,6 @@
 package org.javaopencvbook;
 
 import java.awt.Image;
-import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -10,44 +9,60 @@ import javax.swing.JLabel;
 import org.javaopencvbook.utils.ImageProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.videoio.Videoio;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
 
 public class App {
 
-	static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
-	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
 	private JFrame frame;
 	private JLabel imageLabel;
-	
+
 	public static void main(String[] args) {
 		App app = new App();
 		app.initGUI();
 		app.runMainLoop(args);
 	}
 
-	public void initGUI(){
+	public void initGUI() {
 		frame = new JFrame("Camera Input Example");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,  400);
+		frame.setSize(1024, 768);
 		imageLabel = new JLabel();
 		frame.add(imageLabel);
 		frame.setVisible(true);
 	}
-	
+
 	public void runMainLoop(String[] args){
 		ImageProcessor imageProcessor = new ImageProcessor();
 		Mat webcamMatImage = new Mat();
+		Mat filterImage = new Mat();
 		Image tempImage;
 		VideoCapture capture = new VideoCapture(0);
-		capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 320);
-		capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 240);
+		capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 1024);
+		capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 768);
+		
+		Scalar hsvGreenLower = new Scalar(29, 86, 6);
+		Scalar hsvGreenUpper = new Scalar(64, 255, 255);
+		
+		
 		
 		if(capture.isOpened()){
 			while(true){
 				capture.read(webcamMatImage);
 				if(!webcamMatImage.empty()){
-					tempImage = imageProcessor.toBufferedImage(webcamMatImage);
+					//tempImage = imageProcessor.toBufferedImage(webcamMatImage);
+					Imgproc.GaussianBlur(webcamMatImage, filterImage, new Size(11, 11), 0);
+					Imgproc.cvtColor(filterImage, filterImage, Imgproc.COLOR_BGR2HSV);
+					Core.inRange(filterImage, hsvGreenLower, hsvGreenUpper, filterImage);
+					
+					tempImage = imageProcessor.toBufferedImage(filterImage);
 					ImageIcon imageIcon = new ImageIcon(tempImage, "Captured video");
 					imageLabel.setIcon(imageIcon);
 					frame.pack();
