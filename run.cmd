22c1e0b3583e1e501ddca60eacd839c502946a40
eriplinger <eriplinger@googlemail.com>
2016-07-21 19:19:14 +0200

    changed to 3.1.0, added ant-file, working on windows

---
+++ run.cmd
@@ -0,0 +1,3 @@
+REM ant -DocvJarDir=C:\Users\U10645Ema\workspace\opencv\build\java -DocvLibDir=C:\Users\U10645Ema\workspace\opencv\build\java\x64
+REM ant -DocvJarDir=C:\Users\U10645Ema\workspace\JavaOpenCV\lib -DocvLibDir=C:\Users\U10645Ema\workspace\JavaOpenCV\lib
+ant -DocvJarDir=.\lib -DocvLibDir=.\lib
